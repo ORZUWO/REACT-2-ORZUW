0bbfa34fd5634eca0aa29bb4e68d2c15462a0726
ORZU <orzu3008@gmail.com>
2026-04-11 14:41:34 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {type RootState } from './store/store'
+import { useFormik } from 'formik';
 
 const App = () => {
+  const {data}=useSelector((store:RootState)=>store.todo)
+  const Dispatch=useDispatch()
+  const [idx,setidx]=useState(null)
+
+
+
+const {handleChange,handleSubmit,values,setValues,resetForm} = useFormik({
+     initialValues: {
+       name:"",
+       age:0,
+       status:true,
+       id:Date.now()
+     },
+     onSubmit: values=> {
+       if(idx==null){
+        
+       }
+       else{
+
+       }
+     },
+   });
+
+
   return (
     <>
 
@@ -10,20 +37,6 @@ const App = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-     
-      
     </>
   )
 }
